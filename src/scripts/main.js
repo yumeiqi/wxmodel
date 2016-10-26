7f210e6632308d1125330308c911fa3a7faabc7e
于美琪 <986823092@qq.com>
2016-10-26 16:22:27 +0800

    增加后台自动选择header颜色

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
-var b;
+// var str = '<%hyperlink%>' ;
+// console.log(str);
+
+// var list = eval("([" + str + "])");
+// console.log(list);
+
+//  for (var i = 0; i < list.length; i++) 
+//         {
+//             var item = list[i];
+//             document.write(item.month);
+//    }
+
+// var html = '';
+// for(var i=0;i<5;i++){
+//     if(i==0){
+//         html +='<li class="first"><div class="number">';
+//     }else{
+//         html += '<li class="first active"><div class="number">'
+//     }
+//     html += ='这个是div中的内容</div></li>'
+// }
+// document.getElementById('container').innerHTML = html;
